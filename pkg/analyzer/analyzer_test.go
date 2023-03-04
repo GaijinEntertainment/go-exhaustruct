1fd95de0f0ce58bef58262cd04441daca066af09
xobotyi <xog3@yandex.ru>
2023-03-04 01:57:53 +0100

    tests: add tests for #6
    This issue occurred irrelevant, but still worth adding tests for it

--- pkg/analyzer/analyzer_test.go
+++ pkg/analyzer/analyzer_test.go
@@ -1,7 +1,6 @@
 package analyzer_test
 
 import (
-	"os"
 	"path/filepath"
 	"testing"
 
@@ -10,44 +9,35 @@ import (
 	"github.com/GaijinEntertainment/go-exhaustruct/v2/pkg/analyzer"
 )
 
+var testdataPath, _ = filepath.Abs("../../testdata")
+
 func TestAll(t *testing.T) {
 	t.Parallel()
 
-	wd, err := os.Getwd()
-	if err != nil {
-		t.Fatalf("Failed to get wd: %s", err)
-	}
-
-	testdata := filepath.Join(filepath.Dir(filepath.Dir(wd)), "testdata")
-
 	a, err := analyzer.NewAnalyzer(
-		[]string{".*\\.Test", ".*\\.Test2", ".*\\.Embedded", ".*\\.External"},
+		[]string{".*\\.Test.*", ".*\\.Test2", ".*\\.Embedded", ".*\\.External"},
 		[]string{".*Excluded$"},
 	)
 	if err != nil {
 		t.Error(err)
 	}
 
-	analysistest.Run(t, testdata, a, "s")
+	analysistest.Run(t, testdataPath, a, "s")
 }
 
 func BenchmarkAll(b *testing.B) {
-	wd, err := os.Getwd()
-	if err != nil {
-		b.Fatalf("Failed to get wd: %s", err)
-	}
-
-	testdata := filepath.Join(filepath.Dir(filepath.Dir(wd)), "testdata")
-
 	a, err := analyzer.NewAnalyzer(
-		[]string{".*\\.Test", ".*\\.Test2", ".*\\.Embedded", ".*\\.External"},
+		[]string{".*\\.Test.*", ".*\\.Test2", ".*\\.Embedded", ".*\\.External"},
 		[]string{".*Excluded$"},
 	)
 	if err != nil {
 		b.Error(err)
 	}
 
+	b.ResetTimer()
+	b.ReportAllocs()
+
 	for i := 0; i < b.N; i++ {
-		analysistest.Run(b, testdata, a, "s")
+		analysistest.Run(b, testdataPath, a, "s")
 	}
 }
